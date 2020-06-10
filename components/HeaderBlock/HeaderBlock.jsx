e68da9dc66e73aa07d1bd421f356aaf4589ff1f2
Maxim Remizov <sharkercool@mail.ru>
2020-06-10 14:51:54 +0300

    refactor headerblock same

--- components/HeaderBlock/HeaderBlock.jsx
+++ components/HeaderBlock/HeaderBlock.jsx
@@ -1,11 +1,10 @@
-import LinkButton from "../common/LinkButton/LinkButton"
-import Link from "next/Link"
-import { HeaderStyle, ListStyle } from './style'
+import LinkButton from "../common/LinkButton/LinkButton";
+import { HeaderStyle, ListStyle } from './style';
 
 const HeaderBlock = () => {
     return (
         <HeaderStyle>
-            <Link href='/'><a><img src='logo-white.svg' alt='logo' /></a></Link>
+            <a href='/'><img src='logo-white.svg' alt='logo' /></a>
             <ListStyle>
                 <li><LinkButton name='О нас' link='/about' /></li>
                 <li><LinkButton name='Портфолио' link='/portfolio' /></li>
